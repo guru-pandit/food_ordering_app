0f1925931cd8df3e6d8243d81ca5c085663f4325
UltimateGG18 <gauravgaonkar1998@gmail.com>
2021-09-13 11:00:19 +0530

    modification in menuitem routes and controller(create and get all menuitems)

--- src/controllers/menuitem.controller.js
+++ src/controllers/menuitem.controller.js
@@ -1,1 +1,55 @@
-module.exports = {}
+const { Menuitem, Restaurant, Mealtype } = require("../models");
+const db = require("../models");
+const Op = db.Sequelize.Op;
+
+//to add new menuitem
+const addNewMenuitem = async(req, res) => {
+    try {
+        const menuitem = {
+            name: req.body.name,
+            description: req.body.description,
+            image: req.body.image,
+            price: req.body.price,
+            restaurantId: req.body.restaurantId,
+            mealtypeId: req.body.mealtypeId
+        }
+        const isExist = await Menuitem.findOne({ where: { name: menuitem.name, restaurantId: menuitem.restaurantId } })
+        if (isExist === null) {
+            const menuitemCreated = await Menuitem.create(menuitem)
+            if (menuitemCreated !== null) {
+                return res.status(200).json({ message: "Menuitem Added Successfully", restaurant: menuitemCreated })
+            } else {
+                return res.status(500).json({ error: "Menuitem NOT Added Successfully" })
+            }
+        } else {
+            return res.status(500).json({ message: "Menuitem already Exist" })
+        }
+    } catch (err) {
+        console.log(err);
+        res.status(500).json({ error: "Menuitem NOT Added Successfully", Error: err })
+    }
+}
+
+//to get all Menuitems
+const getMenuitems = async(req, res) => {
+    try {
+        const response = await Menuitem.findAll({
+            include: [{ model: Mealtype, attributes: ["name", "content"] },
+                { model: Restaurant, attributes: ["name", "address", "contact", "locationId"] }
+            ]
+        });
+        //to check if we get response or not
+        /*findAll method returns array of objects because of that we check length of array to ensure 
+        whether array is empty or not*/
+        if (response.length > 0) {
+            return res.status(200).json({ message: "Menuitems Fetched Successfully.", Menuitems: response });
+        } else {
+            return res.status(500).json({ error: "Menuitems Not Fetched Successfully." });
+        }
+    } catch (err) {
+        console.log(err);
+        res.status(500).json({ error: "Menuitems Not Fetched Successfully.", error: err });
+    }
+}
+
+module.exports = { addNewMenuitem, getMenuitems }
--- src/routes/menuitem.route.js
+++ src/routes/menuitem.route.js
@@ -1,3 +1,7 @@
+const menuitemController = require("../controllers").menuitem;
 module.exports = (app) => {
-
+    //to create new menuitem
+    app.post("/api/v1/addnewmenuitem", menuitemController.addNewMenuitem);
+    //to fetch all menuitems
+    app.get("/api/v1/getmenuitems", menuitemController.getMenuitems)
 }
