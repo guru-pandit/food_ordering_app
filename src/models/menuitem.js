273b8e4edd92ac42ada895c4c732e74e458e419e
Guruprasad Pandit <gppandit.gp@gmail.com>
2021-09-13 13:50:11 +0530

    Added model validation to the menuitem

--- src/models/menuitem.js
+++ src/models/menuitem.js
@@ -9,20 +9,54 @@ module.exports = (sequelize, DataTypes) => {
      * This method is not a part of Sequelize lifecycle.
      * The `models/index` file will call this method automatically.
      */
-    static associate({Mealtype,Restaurant}) {
-       // define association here
-      Menuitem.belongsTo(Mealtype,{foreignKey:"mealtypeId"})
-      Menuitem.belongsTo(Restaurant,{foreignKey:"restaurantId"})
-     
+    static associate({ Mealtype, Restaurant }) {
+      // define association here
+      Menuitem.belongsTo(Mealtype, { foreignKey: "mealtypeId" })
+      Menuitem.belongsTo(Restaurant, { foreignKey: "restaurantId" })
+
     }
   };
   Menuitem.init({
-    name: DataTypes.STRING,
-    description: DataTypes.STRING,
-    image: DataTypes.STRING,
-    price: DataTypes.INTEGER,
-    restaurantId: DataTypes.INTEGER,
-    mealtypeId: DataTypes.INTEGER
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notEmpty: { msg: "Name should not be empty" },
+      }
+    },
+    description: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notEmpty: { msg: "Description should not be empty" }
+      }
+    },
+    image: {
+      type: DataTypes.JSON,
+      allowNull: false,
+    },
+    price: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      validate: {
+        notEmpty: { msg: "Price should not be empty" },
+        isNumeric: { msg: "Price should be a number" }
+      }
+    },
+    restaurantId: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      validate: {
+        notEmpty: { msg: "Please select a restaurant" }
+      }
+    },
+    mealtypeId: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      validate: {
+        notEmpty: { msg: "Please select a meal type" }
+      }
+    },
   }, {
     sequelize,
     modelName: 'Menuitem',
