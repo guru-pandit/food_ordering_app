c902ca901a7eacae7458f5da4aa7b916d93c29e2
Guruprasad Pandit <gppandit.gp@gmail.com>
2021-09-22 17:03:42 +0530

    some minor changes in server.js

--- server.js
+++ server.js
@@ -3,12 +3,8 @@ const express = require("express");
 const path = require("path");
 const cors = require("cors");
 const db = require("./src/models");
-const nodemailer = require("nodemailer");
 const cookieParser = require("cookie-parser");
 const session = require("express-session");
-const publicDirectory = path.join(__dirname, './public');
-require("dotenv").config();
-const path = require('path')
 
 global.__basedir = __dirname;
 
@@ -28,10 +24,10 @@ app.use(session({ secret: process.env.SESSION_SECRET_KEY, resave: false, saveUni
 
 //setting view engine
 app.set("views", "views");
-app.set("view engine",'hbs');
+app.set("view engine", 'hbs');
 
 //setting public directory
-const publicDir = path.join(__dirname, "./public");
+const publicDirectory = path.join(__dirname, './public');
 app.use(express.static(publicDir));
 
 // calling sync() method
@@ -46,7 +42,6 @@ require("./src/routes/user.route")(app);
 require("./src/routes/menuitem.route")(app);
 require("./src/routes/order.route")(app);
 require("./src/routes/restaurant.route")(app);
-
 
 // Setting Port and listening for requests
 const PORT = process.env.PORT || 8080;
