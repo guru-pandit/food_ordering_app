01b611e45eb47dba1c3022525467db34f9b2b48a
Guruprasad Pandit <gppandit.gp@gmail.com>
2021-09-29 22:40:33 +0530

    binded search api

--- src/routes/restaurant.route.js
+++ src/routes/restaurant.route.js
@@ -2,6 +2,8 @@ const restaurantController = require("../controllers").restaurant;
 const { uploadImage } = require("../services/upload.service");
 
 module.exports = (app) => {
+   // Home page
+   app.get("/api/v1", restaurantController.home)
    //to get all restaurant
    app.get("/api/v1/restaurants", restaurantController.getAllRestaurants)
    //to get restaurants by location
@@ -13,8 +15,8 @@ module.exports = (app) => {
    //to filter restaurant
    app.post("/api/v1/filterrestaurants", restaurantController.filterRestaurant)
    //to search restaurant
-   // app.post("/api/v1/searchrestaurants", restaurantController.searchRestaurant)
-   app.get("/api/v1/searchrestaurants", restaurantController.searchRestaurant)
+   app.post("/api/v1/searchrestaurants", restaurantController.searchRestaurant)
+   // app.get("/api/v1/searchrestaurants", restaurantController.searchRestaurant)
    //to add opening and closing time
    app.post("/api/v1/addtime/:restaurantId", restaurantController.addTime);
    //to add iamge
