c89375fdcf150d3aae797a322a4052df9944819a
Guruprasad Pandit <gppandit.gp@gmail.com>
2021-10-04 22:15:38 +0530

    Bound filter api

--- src/controllers/mealtype.controller.js
+++ src/controllers/mealtype.controller.js
@@ -1,4 +1,6 @@
 const { Mealtype } = require("../models");
+const db = require("../models");
+const Op = db.Sequelize.Op;
 
 // Function to get all mealtypes
 const getAllMealtypes = async (req, res) => {
@@ -19,7 +21,18 @@ const getAllMealtypes = async (req, res) => {
 // Funtion to search mealtypes
 const searchMealtypes = async (req, res) => {
     try {
+        let { search } = req.query
+        let mealtypes = await Mealtype.findAll({
+            where: {
+                name: { [Op.like]: '%' + search + '%' }
+            },
+        })
 
+        if (mealtypes.length > 0) {
+            return res.status(200).json({ mealtypes })
+        } else {
+            return res.status(400).json({ message: "Mealtypes not found" })
+        }
     } catch (err) {
         res.status(500).json({ error: err.message || "Something went wrong" })
     }
