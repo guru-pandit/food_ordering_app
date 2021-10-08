eda4f6436c58af84ff4a2031ee34c9fe14875f78
Guruprasad Pandit <gppandit.gp@gmail.com>
2021-10-08 10:46:01 +0530

    added dynamic images to restaurant details page

--- src/routes/restaurant.route.js
+++ src/routes/restaurant.route.js
@@ -9,7 +9,7 @@ module.exports = (app) => {
    //to get restaurants by location
    app.get("/api/v1/restaurantsbylocation/:locationId", restaurantController.getRestaurantsByLocation)
    //to get restaurants details by restaurantId
-   app.get("/api/v1/retaurantsdetails", restaurantController.getRestaurantsDetails)
+   app.get("/api/v1/restaurantsdetails", restaurantController.getRestaurantsDetails)
    //to add review
    app.post("/api/v1/addnewreview", restaurantController.addReview)
    //to filter restaurant
@@ -20,5 +20,5 @@ module.exports = (app) => {
    //to add opening and closing time
    app.post("/api/v1/addtime/:restaurantId", restaurantController.addTime);
    //to add iamge
-   app.post("/api/v1/restaurantImage/:restaurantId", uploadImage.single('image'), restaurantController.addImage);
+   app.post("/api/v1/restaurantImage/:restaurantId", uploadImage.array('images', 5), restaurantController.addImage);
 }
