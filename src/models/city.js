775852832b8327588ea11ac5eeb708b86ffddc42
Gaurav Gavkar <gauravgaonkar1998@gmail.com>
2021-10-25 16:28:25 +0530

    changes in index.js

--- src/models/city.js
+++ src/models/city.js
@@ -9,7 +9,7 @@ module.exports = (sequelize, DataTypes) => {
      * This method is not a part of Sequelize lifecycle.
      * The `models/index` file will call this method automatically.
      */
-    static associate({State,Location}) {
+    static associate({ State, Location }) {
       // define association here
       City.belongsTo(State, { foreignKey: "stateId" });
       City.hasMany(Location, { foreignKey: "cityId" })
--- src/models/index.js
+++ src/models/index.js
@@ -9,8 +9,9 @@ const env = process.env.NODE_ENV || 'development';
 const config = require(__dirname + '/../config/config.json')[env];
 const db = {};
 
-console.log("IndexJS(env)", env);
-console.log("IndexJS(config)", config);
+//console.log("IndexJS(env)", env);
+//console.log("IndexJS(config)", config);
+//console.log("IndexJS(config.use_env_variable)", config.use_env_variable);
 
 let sequelize;
 if (config.use_env_variable) {
@@ -25,17 +26,26 @@ fs
     return (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js');
   })
   .forEach(file => {
+    //console.log("files: ", file)
     const model = require(path.join(__dirname, file))(sequelize, Sequelize.DataTypes);
+    //console.log("model: ", model)
+    //console.log("modelname: ", model.name)
     db[model.name] = model;
+    //console.log(db)
   });
-
+//console.log(Object.keys(db))
 Object.keys(db).forEach(modelName => {
+  //console.log(modelName)
   if (db[modelName].associate) {
+    //console.log(db[modelName].associate)
     db[modelName].associate(db);
+    //console.log(db[modelName].associate(db))
   }
 });
-
+//console.log("second last db : ", db)
 db.sequelize = sequelize;
+//console.log("final db: ", db)
 db.Sequelize = Sequelize;
+//console.log("final db: ", db)
 
 module.exports = db;
